ddd98599cc43689ff3bc8817b94a7e48ed40dd84
Shakeel Ilyas <mshakeel117db@gmail.com>
2025-05-15 11:39:55 +0500

    Uploading my project on github.

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,178 @@
+import {
+  AsyncPipe,
+  CurrencyPipe,
+  DatePipe,
+  NgClass,
+  NgFor,
+  NgIf,
+  PercentPipe,
+  UpperCasePipe,
+} from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { StudentService } from '../Services/student.service';
+import { Student } from '../Models/Student';
+import { PercentagePipe } from '../Pipes/percentage.pipe';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  imports: [
+    NgIf,
+    NgFor,
+    NgClass,
+    UpperCasePipe,
+    CurrencyPipe,
+    DatePipe,
+    PercentagePipe,
+    FormsModule,
+    AsyncPipe,
+  ],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.css',
+})
+export class AdminComponent implements OnInit {
+  studentService: StudentService = inject(StudentService);
+
+  isEditing: boolean = false;
+  isInserting: boolean = false;
+  stdIdToEdit: number;
+
+  students: Student[];
+  totalMarks: number;
+
+  filterText: string = 'All';
+
+  searchBoxClasses: boolean = true;
+
+  totalStudents = new Promise((resolve, rejection) => {
+    setTimeout(() => {
+      resolve(this.studentService.students.length);
+    }, 2000);
+  });
+
+  //PROPERTIES FOR INSERTING
+  @ViewChild('name') Name: ElementRef;
+  @ViewChild('gender') Gender: ElementRef;
+  @ViewChild('dob') Dob: ElementRef;
+  @ViewChild('course') Course: ElementRef;
+  @ViewChild('marks') Marks: ElementRef;
+  @ViewChild('fee') Fee: ElementRef;
+
+  //PROPERTIES FOR EDITING
+  @ViewChild('editName') editName: ElementRef;
+  @ViewChild('editGender') editGender: ElementRef;
+  @ViewChild('editDob') editDob: ElementRef;
+  @ViewChild('editCourse') editCourse: ElementRef;
+  @ViewChild('editMarks') editMarks: ElementRef;
+  @ViewChild('editFee') editFee: ElementRef;
+
+  ngOnInit() {
+    this.students = this.studentService.filterStudentByGender(this.filterText);
+    this.totalMarks = this.studentService.totalMarks;
+  }
+
+  onFilterValueChanged(event: any) {
+    this.filterText = event.target.value;
+    this.students = this.studentService.filterStudentByGender(this.filterText);
+    this.totalStudents = new Promise((resolve, rejection) => {
+      resolve(this.students.length);
+    });
+  }
+
+  back(searchBox: HTMLInputElement) {
+    this.searchBoxClasses = true;
+    searchBox.placeholder = 'Search By Name';
+
+    this.students = this.studentService.filterStudentByGender(this.filterText);
+
+    this.totalStudents = new Promise((resolve, rejection) => {
+      resolve(this.students.length);
+    });
+  }
+
+  filterStudentByName(searchBox: HTMLInputElement) {
+    if (searchBox.value != '') {
+      this.searchBoxClasses = true;
+      searchBox.placeholder = 'Search By Name';
+
+      this.students = this.studentService.filterStudentByGender(
+        this.filterText
+      );
+      this.students = this.students.filter((student) => {
+        return student.name
+          .toLowerCase()
+          .includes(searchBox.value.toLowerCase());
+      });
+
+      searchBox.value = '';
+
+      this.totalStudents = new Promise((resolve, rejection) => {
+        resolve(this.students.length);
+      });
+    } else {
+      searchBox.placeholder = 'Error: Enter Value';
+      this.searchBoxClasses = false;
+    }
+  }
+
+  OnInsertClicked() {
+    this.isInserting = true;
+  }
+  OnInsertCancelled() {
+    this.isInserting = false;
+  }
+  OnInsertSaved() {
+    this.studentService.CreateStudent(
+      this.Name.nativeElement.value,
+      this.Gender.nativeElement.value,
+      this.Dob.nativeElement.value,
+      this.Course.nativeElement.value,
+      this.Marks.nativeElement.value,
+      this.Fee.nativeElement.value
+    );
+    // this.students = this.studentService.students;
+    this.isInserting = false;
+
+    // this.students = this.studentService.filterStudentByGender(this.filterText);
+    this.totalStudents = new Promise((resolve, rejection) => {
+      resolve(this.students.length);
+    });
+  }
+
+  OnEditClicked(stdId: number) {
+    this.isEditing = true;
+    this.stdIdToEdit = stdId;
+  }
+
+  onDeleteClicked(studentId: number) {
+    this.studentService.DeleteStudent(studentId);
+    this.students = this.studentService.filterStudentByGender(this.filterText);
+    this.totalStudents = new Promise((resolve, rejection) => {
+      resolve(this.students.length);
+    });
+  }
+
+  OnEditCancelled() {
+    this.isEditing = false;
+  }
+
+  OnEditSaved(student: Student) {
+    student.name = this.editName.nativeElement.value;
+    student.gender = this.editGender.nativeElement.value;
+    student.dob = this.editDob.nativeElement.value;
+    student.course = this.editCourse.nativeElement.value;
+    student.marks = this.editMarks.nativeElement.value;
+    student.fee = this.editFee.nativeElement.value;
+
+    this.isEditing = false;
+
+    this.students = this.studentService.filterStudentByGender(this.filterText);
+  }
+}
